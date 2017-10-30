e2493e2fd4ef3b6976a94e1edb5083291705934b
AJ Hurtado <ajhurtado3@gmail.com>
2017-10-29 21:01:45 -0700

    Updated with start of HW2

--- src/HW2/Maze.java
+++ src/HW2/Maze.java
@@ -1,8 +1,42 @@
-//package HW2;
-//
-//public class Maze {
-//
-//    public Maze (File fileName) {
-//
-//    }
-//}
+package HW2;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Maze {
+
+    private double[][] maze;
+    int rows;
+    int cols;
+    int startPoint1;
+    int startPoint2;
+    int endPoint1;
+    int endPoint2;
+
+    public Maze (File fileName) throws IOException {
+
+        Scanner inputScanner = new Scanner (fileName);
+
+        while(inputScanner.hasNext()) {
+            rows = inputScanner.nextInt();
+            cols = inputScanner.nextInt();
+            inputScanner.hasNextLine();
+            startPoint1 = inputScanner.nextInt();
+            startPoint2 = inputScanner.nextInt();
+            inputScanner.hasNextLine();
+            endPoint1 = inputScanner.nextInt();
+            endPoint2 = inputScanner.nextInt();
+            inputScanner.hasNextLine();
+            for (int i = 0; i < rows; i++) {
+                String line = inputScanner.nextLine();
+                System.out.println(line);
+                for (int j = 0; j < cols; j ++) {
+                    maze[i][j] = line.charAt(j);
+                }
+            }
+        }
+    }
+
+
+}
