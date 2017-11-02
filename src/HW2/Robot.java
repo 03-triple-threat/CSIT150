8dd38e6d05d14919513deaecd8cd8cbe8f489ad4
ajhurtado3 <ajhurtado3@gmail.com>
2017-11-02 03:29:03 -0500

    Updated 11/02/17 3:28am - Maze class complete exlcuding toString method, Robot class started

---
+++ src/HW2/Robot.java
@@ -0,0 +1,22 @@
+package HW2;
+
+/**
+ * Created by ajhurtado3 on 11/2/17.
+ */
+public class Robot {
+
+    private int currentRow;
+    private int currentCol;
+
+    public Robot (Maze maze) {
+
+        Robot r = new Robot(maze);
+    }
+
+    public int chooseMoveDirection() {}
+
+    public boolean move(int direction) {}
+
+    public boolean solved() {}
+
+}
