e83db7c1aae1c22a6633cd33b6ba538635be82d9
ajhurtado3 <ajhurtado3@gmail.com>
2017-11-10 11:16:55 -0600

    Updated with HW2/3

--- src/HW2/RightHandRobot.java
+++ src/HW2/RightHandRobot.java
@@ -5,60 +5,127 @@ public class RightHandRobot extends Robot {
     int face;
 
 
-        public RightHandRobot(Maze maze) {
-            super(maze);
+    public RightHandRobot(Maze maze) {
+        super(maze);
 
-            face = 2;
-
-        }
-
-        public int chooseMoveDirection() {
-
-            return (int) (Math.random() * 4);
-        }
-
-        public boolean move(int direction) {
-            boolean robotMove = false;
-
-
-            switch (direction) {
-                case 0:
-                    if (mazeTest.openCell(currentRow - 1, currentCol)) {
-                        mazeTest.setCell(currentRow, currentCol, ' ');
-                        currentRow--;
-                        mazeTest.setCell(currentRow, currentCol, 'r');
-                        robotMove = true;
-                        //move(direction);
-                    }
-                    break;
-                case 1:
-                    if (mazeTest.openCell(currentRow, currentCol + 1)) {
-                        mazeTest.setCell(currentRow, currentCol, ' ');
-                        currentCol++;
-                        mazeTest.setCell(currentRow, currentCol, 'r');
-                        robotMove = true;
-                    }
-                    break;
-                case 2:
-                    if (mazeTest.openCell(currentRow + 1, currentCol)) {
-                        mazeTest.setCell(currentRow, currentCol, ' ');
-                        currentRow++;
-                        face=2;
-                        robotMove = true;
-                    }
-                    break;
-                case 3:
-                    if (mazeTest.openCell(currentRow, currentCol - 1)) {
-                        mazeTest.setCell(currentRow, currentCol, ' ');
-                        currentCol--;
-                        mazeTest.setCell(currentRow, currentCol, 'r');
-                        robotMove = true;
-                    }
-                    break;
-            }
-            return robotMove;
-        }
+        face = 2;
 
     }
 
+    public int chooseMoveDirection() {
+
+        if (face == 2) {
+            if (mazeTest.openCell(currentRow, currentCol - 1)) {
+                face = 3;
+                return face;
+            } else if (mazeTest.openCell(currentRow + 1, currentCol)) {
+                face = 2;
+                return face;
+            } else if (mazeTest.openCell(currentRow, currentCol + 1)) {
+                face = 1;
+                return face;
+            } else if (mazeTest.openCell(currentRow - 1, currentCol)) {
+                face = 0;
+                return face;
+            }
+
+        } else if (face == 3) {
+            if (mazeTest.openCell(currentRow - 1, currentCol)) {
+                face = 0;
+                return face;
+            } else if (mazeTest.openCell(currentRow, currentCol - 1)) {
+                face = 3;
+                return face;
+            } else if (mazeTest.openCell(currentRow + 1, currentCol)) {
+                face = 2;
+            } else if (mazeTest.openCell(currentRow, currentCol + 1)) {
+                face = 1;
+                return face;
+            }
+        } else if (face == 0)
+
+        {
+            if (mazeTest.openCell(currentRow, currentCol + 1)) {
+                face = 1;
+                return face;
+            } else if (mazeTest.openCell(currentRow - 1, currentCol)) {
+                    face = 0;
+                    return face;
+                }
+            } else if (mazeTest.openCell(currentRow, currentCol - 1)) {
+                    face = 3;
+                    return face;
+                }
+             else if (mazeTest.openCell(currentRow + 1, currentCol)) {
+                    face = 2;
+                    return face;
+                }
+        } else if (face == 1)
+
+        {
+            if (mazeTest.openCell(currentRow + 1, currentCol)) {
+                face = 2;
+                return face;
+            } else if (!mazeTest.openCell(currentRow + 1, currentCol)) {
+                if (mazeTest.openCell(currentRow, currentCol + 1)) {
+                    face = 1;
+                    return face;
+                }
+            } else if (!(mazeTest.openCell(currentRow + 1, currentCol) && (mazeTest.openCell(currentRow, currentCol + 1)))) {
+                if (mazeTest.openCell(currentRow - 1, currentCol)) {
+                    face = 0;
+                    return face;
+                }
+            } else if (!(mazeTest.openCell(currentRow + 1, currentCol) && (mazeTest.openCell(currentRow, currentCol + 1) &&
+                    (mazeTest.openCell(currentRow - 1, currentCol))))) {
+                face = 3;
+                return face;
+            }
+        }
+        return face;
+    }
+
+    public boolean move(int direction) {
+        boolean robotMove = false;
+
+
+        switch (direction) {
+            case 0:
+                if (mazeTest.openCell(currentRow - 1, currentCol)) {
+                    mazeTest.setCell(currentRow, currentCol, ' ');
+                    currentRow--;
+                    mazeTest.setCell(currentRow, currentCol, 'r');
+                    robotMove = true;
+                    //move(direction);
+                }
+                break;
+            case 1:
+                if (mazeTest.openCell(currentRow, currentCol + 1)) {
+                    mazeTest.setCell(currentRow, currentCol, ' ');
+                    currentCol++;
+                    mazeTest.setCell(currentRow, currentCol, 'r');
+                    robotMove = true;
+                }
+                break;
+            case 2:
+                if (mazeTest.openCell(currentRow + 1, currentCol)) {
+                    mazeTest.setCell(currentRow, currentCol, ' ');
+                    currentRow++;
+                    face = 2;
+                    robotMove = true;
+                }
+                break;
+            case 3:
+                if (mazeTest.openCell(currentRow, currentCol - 1)) {
+                    mazeTest.setCell(currentRow, currentCol, ' ');
+                    currentCol--;
+                    mazeTest.setCell(currentRow, currentCol, 'r');
+                    robotMove = true;
+                }
+                break;
+        }
+        return robotMove;
+    }
+
 }
+
